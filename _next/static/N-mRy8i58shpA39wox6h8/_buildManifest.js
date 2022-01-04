68f58be1a4f66131160e12fa2bd3d7ed9a04150e
mactkg <mactkg@users.noreply.github.com>
2022-01-04 10:01:38 +0000

    deploy: cbc831588291b20e6eae58d10d231a5d6c5ff391

--- _next/static/N-mRy8i58shpA39wox6h8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4306361b97c0b916.css","static/chunks/pages/index-548699ccce4b70db.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
