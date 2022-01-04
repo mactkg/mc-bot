57d6e4590a63bb9cf85e6fe52acfa135292e238a
mactkg <mactkg@users.noreply.github.com>
2022-01-04 10:39:53 +0000

    deploy: 3d6e827bdbd045f66239c4be7c26a52ab5d3c4c0

---
+++ _next/static/bKp7QwoBRmmWYTz6_UmfA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4306361b97c0b916.css","static/chunks/pages/index-91946d7438c8204a.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/kXLbWAhMhlRG0bc24TaPq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4306361b97c0b916.css","static/chunks/pages/index-6d32f98976dee99c.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
